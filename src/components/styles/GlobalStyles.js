80a7b0aac68885fb2422848f47ff6438a365b963
unknown <andreas.alfven95@gmail.com>
2020-11-21 21:40:33 +0100

    fix font

--- src/components/styles/GlobalStyles.js
+++ src/components/styles/GlobalStyles.js
@@ -2,8 +2,7 @@ import { createGlobalStyle } from "styled-components"
 
 export const GlobalStyle = createGlobalStyle`
     * {
-        @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
-        font-family: "Roboto", serif;
+        font-family: Roboto, sans-serif;
         margin: 0; 
         padding: 0;
         box-sizing: border-box;
